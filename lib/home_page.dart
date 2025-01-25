cf68a3c92d4cdd910f91d8e2cb1d467497ac7a7e
Murshidul <murshidulh@gmail.com>
2025-01-25 10:46:44 +0600

    Shimmer used and UI designed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:assignment_twenty_two/home_controller.dart';
 import 'package:assignment_twenty_two/product_model.dart';
+import 'package:assignment_twenty_two/product_shimmer.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -13,28 +14,88 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Get API Integration"),
+        title: Text("Products"),
       ),
       body: GetBuilder<HomeController>(
         builder: (_) {
           return homeController.isLoading
-              ? Center(child: CircularProgressIndicator())
-              : ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: homeController.products.length,
-                  itemBuilder: (context, index) {
-                    ProductModel product = homeController.products[index];
-                    return Card(
-                      child: ListTile(
-                        leading: Image.network(product.image!),
-                        title: Text(
-                          product.title!,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        subtitle: Text(product.description!),
+              ? ProductShimmer()
+              : Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10,
                       ),
-                    );
-                  });
+                      shrinkWrap: true,
+                      itemCount: homeController.products.length,
+                      itemBuilder: (context, index) {
+                        ProductModel product = homeController.products[index];
+                        return Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: [
+                                Image.network(
+                                  product.image!,
+                                  height: 80,
+                                  width: 80,
+                                  fit: BoxFit.cover,
+                                ),
+                                SizedBox(
+                                  height: 8.0,
+                                ),
+                                Text(
+                                  product.title!,
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 7,
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Text(
+                                          "Price",
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        Text(
+                                          product.price.toString(),
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      children: [
+                                        Icon(
+                                          Icons.star,
+                                          color: Colors.orange,
+                                          size: 18.0,
+                                        ),
+                                        Text(
+                                          product.rating!.rate.toString(),
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      }),
+                );
         },
       ),
     );
---
+++ lib/product_shimmer.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class ProductShimmer extends StatelessWidget {
+  const ProductShimmer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Shimmer.fromColors(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  height: 150,
+                  width: 150,
+                ),
+                baseColor: Colors.grey[300]!,
+                highlightColor: Colors.grey[100]!,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Shimmer.fromColors(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  height: 150,
+                  width: 150,
+                ),
+                baseColor: Colors.grey[300]!,
+                highlightColor: Colors.grey[100]!,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Shimmer.fromColors(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  height: 150,
+                  width: 150,
+                ),
+                baseColor: Colors.grey[300]!,
+                highlightColor: Colors.grey[100]!,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Shimmer.fromColors(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  height: 150,
+                  width: 150,
+                ),
+                baseColor: Colors.grey[300]!,
+                highlightColor: Colors.grey[100]!,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Shimmer.fromColors(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  height: 150,
+                  width: 150,
+                ),
+                baseColor: Colors.grey[300]!,
+                highlightColor: Colors.grey[100]!,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Shimmer.fromColors(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  height: 150,
+                  width: 150,
+                ),
+                baseColor: Colors.grey[300]!,
+                highlightColor: Colors.grey[100]!,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Shimmer.fromColors(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  height: 150,
+                  width: 150,
+                ),
+                baseColor: Colors.grey[300]!,
+                highlightColor: Colors.grey[100]!,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Shimmer.fromColors(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  height: 150,
+                  width: 150,
+                ),
+                baseColor: Colors.grey[300]!,
+                highlightColor: Colors.grey[100]!,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
